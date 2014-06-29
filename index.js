5c037d581dac932e9a443ca7177f136b577ebeb6
Kai Davenport <kaiyadavenport@gmail.com>
2014-06-29 19:16:35 +0000

    loadPages

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var EventEmitter = require('events').EventEmitter
+var parser = require('markdown-parse')
+var fs = require('fs')
+var async = require('async')
 var util = require('util')
 var globby = require('globby')
 
@@ -11,23 +14,55 @@ function BookMaker(source, options){
 
 util.inherits(BookMaker, EventEmitter)
 
-BookMaker.prototype.loadConfigs = function(glob, done){
-	var self = this;
+// an array of relative glob results
+BookMaker.prototype.globs = function(glob, done){
 	if(typeof(glob)==='string'){
 		glob = [glob]
 	}
 	globby(glob, {
 		cwd:self._source
-	}, function(err, files){
+	}, done)
+}
+
+// an array of absolute file paths from globs
+BookMaker.prototype.files = function(glob, done){
+	var self = this;
+	this.globs(glob, function(err, files){
+		if(err) return done(err)
+		files = files.map(function(file){
+			return self._source + '/' + file
+		})
+		done(null, files)
+	})
+}
+
+BookMaker.prototype.loadPages = function(glob, done){
+	var self = this;
+	self.files(glob, function(err, files){
+
+		async.map(files, function(file, nextFile){
+			fs.readFile(file, 'utf8', function(err, markdown){
+				if(err) return nextFile(err)
+				parser(content, nextFile)
+			})
+		}, done)
+
+	})
+}
+
+BookMaker.prototype.loadConfigs = function(glob, done){
+	var self = this;
+	self.files(glob, function(err, files){
 		if(err) return done(err)
 		var config = {}
-		files.forEach(function(file){
-			if(file.match(/\.json/)){
-				var data = require(self._source + '/' + file)
-				Object.keys(data || {}).forEach(function(key){
-					config[key] = data[key]
-				})
-			}
+		files.filter(function(file){
+			return file.match(/\.json/)
+		}).map(function(file){
+			return require(file)
+		}).forEach(function(data){
+			Object.keys(data || {}).forEach(function(key){
+				config[key] = data[key]
+			})
 		})
 		done(null, config)
 	})
