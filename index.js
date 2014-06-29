bb75423072a29cda6dfce1e99f2cf082cd4ebd23
Kai Davenport <kaiyadavenport@gmail.com>
2014-06-29 19:08:22 +0000

    load config test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var EventEmitter = require('events').EventEmitter
 var util = require('util')
+var globby = require('globby')
 
 function BookMaker(source, options){
   EventEmitter.call(this)
@@ -10,7 +11,27 @@ function BookMaker(source, options){
 
 util.inherits(BookMaker, EventEmitter)
 
-
+BookMaker.prototype.loadConfigs = function(glob, done){
+	var self = this;
+	if(typeof(glob)==='string'){
+		glob = [glob]
+	}
+	globby(glob, {
+		cwd:self._source
+	}, function(err, files){
+		if(err) return done(err)
+		var config = {}
+		files.forEach(function(file){
+			if(file.match(/\.json/)){
+				var data = require(self._source + '/' + file)
+				Object.keys(data || {}).forEach(function(key){
+					config[key] = data[key]
+				})
+			}
+		})
+		done(null, config)
+	})
+}
 
 module.exports = function(source, options){
   return new BookMaker(source, options)
--- test.js
+++ test.js
@@ -5,8 +5,16 @@ tape('load the configs', function(t){
 	var book = BookMaker(__dirname + '/book')
 
 	book.loadConfigs('*.json', function(err, config){
-		console.log('-------------------------------------------');
-		console.dir(config)
+		if(err){
+			t.fail(err, 'load config')
+			t.end()
+			return
+		}
+		t.deepEqual(config, {
+			test:'yes',
+			fruit:'apples',
+			color:'red'
+		})
 		t.end()
 	})
 })
