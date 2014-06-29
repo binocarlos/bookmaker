8c884005014fe877c6ee9843c7a4f0b465cfc4cd
Kai Davenport <kaiyadavenport@gmail.com>
2014-06-29 19:47:05 +0000

    copyFiles implementation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var fs = require('fs')
 var async = require('async')
 var util = require('util')
 var globby = require('globby')
+var path = require('path')
 
 function BookMaker(source, options){
   EventEmitter.call(this)
@@ -31,12 +32,25 @@ BookMaker.prototype.files = function(glob, done){
 	this.globs(glob, function(err, files){
 		if(err) return done(err)
 		files = files.map(function(file){
-			return self._source + '/' + file
+			return path.normalize(self._source + '/' + file)
 		})
 		done(null, files)
 	})
 }
 
+BookMaker.prototype.fileCopies = function(glob, dest, done){
+	var self = this;
+	this.globs(glob, function(err, files){
+		if(err) return done(err)
+		files = files.map(function(file){
+			return {
+				source:path.normalize(self._source + '/' + file),
+				dest:path.normalize(dest + '/' + file)
+			}
+		})
+		done(null, files)
+	})
+}
 
 BookMaker.prototype.load = function(configGlob, pagesGlob, done){
 	var self = this;
@@ -85,6 +99,23 @@ BookMaker.prototype.loadConfigs = function(glob, done){
 			})
 		})
 		done(null, config)
+	})
+}
+
+
+BookMaker.prototype.copyFiles = function(glob, dest, done){
+	var self = this;
+	if(!fs.existsSync(dest)){
+		return done(dest + ' does not exist')
+	}
+	self.fileCopies(glob, function(err, files){
+		if(err) return done(err)
+		async.forEach(files, function(file, next){
+			var source = fs.createReadStream(file.source)
+			var dest = fs.createWriteStream(file.dest)
+			dest.on('error', next)
+			dest.on('close', next)
+		}, done)
 	})
 }
 
